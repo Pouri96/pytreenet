fix normalization

pytreenet/time_evolution/tdvp_algorithms/secondorderonesite.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import List, Union
 from copy import deepcopy
 from tqdm import tqdm
-
+import numpy as np
 from ...operators.tensorproduct import TensorProduct
 from ...ttno.ttno import TTNO
 from ...ttns import TreeTensorNetworkState , normalize_ttn_Lindblad
@@ -42,6 +42,7 @@ class SecondOrderOneSiteTDVP(OneSiteTDVP):
                  initial_tol: float = 1e-5,
                  tol_step: float = 1e-1, 
                  max_bond: int = 32, 
+                 norm_tol: float = np.inf,
                  KrylovBasisMode : KrylovBasisMode = KrylovBasisMode.apply_ham,                  
                  config: Union[TTNTimeEvolutionConfig,None] = None) -> None:
         """
@@ -68,9 +69,10 @@ class SecondOrderOneSiteTDVP(OneSiteTDVP):
                          max_bond,
                          KrylovBasisMode,  
                          config)
+        self.norm_tol = norm_tol
         self.backwards_update_path = self._init_second_order_update_path()
         self.backwards_orth_path = self._init_second_order_orth_path()
-        self.state , self.hamiltonian , self.two_neighbour_form_dict = self._init_two_neighbour_form()
+        self.state , self.hamiltonian = self._init_two_neighbour_form()
 
     def _init_two_neighbour_form(self):
         """
@@ -88,7 +90,8 @@ class SecondOrderOneSiteTDVP(OneSiteTDVP):
         self.orthogonalization_path = self._find_tdvp_orthogonalization_path(self.update_path) 
         self.backwards_update_path = self._init_second_order_update_path()
         self.backwards_orth_path = self._init_second_order_orth_path() 
-        return self.state , self.hamiltonian , dict1
+        self.two_neighbour_form_dict = dict1
+        return self.state , self.hamiltonian
     
     def _init_second_order_update_path(self) -> List[str]:
         """
@@ -221,24 +224,10 @@ class SecondOrderOneSiteTDVP(OneSiteTDVP):
         self.backward_sweep()
 
 
-    def forward_sweep_ex(self, j , tol_step):
+    def forward_sweep_ex(self):
         """
         Perform the forward sweep through the state.
         """
-        if j % self.expansion_steps == 0 and self.state.max_bond_dim() <= self.max_bond:
-            tol = self.initial_tol * self.tol_step ** tol_step
-            #state_copy = deepcopy(self.state)
-            state_ex = expand_subspace(self.state, self.hamiltonian, 
-                                        self.num_vecs, self.tau, 
-                                        self.SVDParameters, tol, self.KrylovBasisMode)
-            # if state_ex.max_bond_dim() <= self.max_bond:
-            self.state = state_ex
-            #self.state = adjust_ttn1_structure_to_ttn2( state_ex, state_copy)
-            self.state.move_orthogonalization_center(self.update_path[0],mode = SplitMode.KEEP)
-            self.partial_tree_cache = PartialTreeCachDict()
-            self._init_partial_tree_cache()
-        else:
-            assert self.state.orthogonality_center_id == self.update_path[0]
         for i, node_id in enumerate(self.update_path[:-1]):
             # Orthogonalize
             if i>0:
@@ -248,7 +237,7 @@ class SecondOrderOneSiteTDVP(OneSiteTDVP):
             # Update
             self._update_forward_site_and_link(node_id, next_node_id)
 
-    def run_one_time_step_ex(self, j , tol_step):
+    def run_one_time_step_ex(self):
         """
         Run a single second order time step.
         
@@ -256,7 +245,7 @@ class SecondOrderOneSiteTDVP(OneSiteTDVP):
         tree.
         """
 
-        self.forward_sweep_ex(j, tol_step)
+        self.forward_sweep_ex()
         self._final_forward_update()
         self.backward_sweep()  
     
@@ -278,30 +267,54 @@ class SecondOrderOneSiteTDVP(OneSiteTDVP):
                 the path to that file can be specified here. Defaults to "".
             pgbar (bool, optional): Toggles the progress bar. Defaults to True.
         """
+      
         self._init_results(evaluation_time)
         assert self._results is not None
         tol_step = -1
         for i in tqdm(range(self.num_time_steps + 1), disable=not pgbar):
-            if i != 0:  # We also measure the initial expectation_values
-                if i % self.expansion_steps == 0:
-                    tol_step += 1
-                #state_copy = deepcopy(self.state)    
-                self.run_one_time_step_ex(i, tol_step)      
-                #self.state = adjust_ttn1_structure_to_ttn2(self.state, state_copy)
+            if i != 0:  # We also measure the initial expectation_values   
+
+                ###########
+                tol_step += 1
+                if i % self.expansion_steps == 0 :
+                    if state_bond.max_bond_dim() < self.max_bond:
+                        tol = self.initial_tol * self.tol_step ** tol_step
+                        state_ex = expand_subspace(state_ex, self.hamiltonian, 
+                                                    self.num_vecs, self.tau, 
+                                                    self.SVDParameters, tol, self.KrylovBasisMode)
+                        self.state = state_ex
+                        self.state.move_orthogonalization_center(self.update_path[0],mode = SplitMode.KEEP)
+                        self.partial_tree_cache = PartialTreeCachDict()
+                        self._init_partial_tree_cache()
+                    else:
+                        assert self.state.orthogonality_center_id == self.update_path[0]   
+                ##########
+
+                self.run_one_time_step_ex() 
+                state_ex = deepcopy(self.state) 
+            ttn = deepcopy(self.state)
+            ttn = original_form(ttn , self.two_neighbour_form_dict)
+            I = TTNO.Identity(ttn)
+            I_ex = ttn.operator_expectation_value_Lindblad(I)
+            norm = I_ex
+            if np.abs(np.abs(I_ex) - 1)  > self.norm_tol :
+                self.state = normalize_ttn_Lindblad(ttn)     
+            else:
+                self.state = ttn                   
             if evaluation_time != "inf" and i % evaluation_time == 0 and len(self._results) > 0:
-                if self.Lindblad :
-                    ttn = deepcopy(self.state)
-                    ttn = original_form(ttn , self.two_neighbour_form_dict)
-                    ttn = normalize_ttn_Lindblad(ttn)
-                    self.state = normalize_ttn_Lindblad(ttn) 
                 index = i // evaluation_time
-                current_results = self.evaluate_operators()
+                current_results = self.evaluate_operators() / norm
                 self._results[0:-1, index] = current_results
                 # Save current time
-                self._results[-1, index] = i*self.time_step_size
-                max_two_neighbour_form(self.state , self.two_neighbour_form_dict)
-                self.partial_tree_cache = PartialTreeCachDict()
-                self._init_partial_tree_cache()              
+                self._results[-1, index] = i*self.time_step_size  
+            max_two_neighbour_form(self.state , self.two_neighbour_form_dict)
+
+            state_bond = deepcopy(self.state)
+            self.record_bond_dimensions()
+            
+            self._orthogonalize_init()
+            self.partial_tree_cache = PartialTreeCachDict()
+            self._init_partial_tree_cache()                 
         
         if evaluation_time == "inf":
             current_results = self.evaluate_operators()

pytreenet/ttns/ttns.py
@@ -153,9 +153,44 @@ def normalize_ttn(ttn: TreeTensorNetworkState , to_copy = False):
 def normalize_ttn_Lindblad(ttn) : 
     ttn_normalized = copy_object(ttn, deep = True)
     I = TTNO.Identity(ttn_normalized)
-    for node_id in list(ttn_normalized.nodes.keys()):
+    norm = ttn_normalized.operator_expectation_value_Lindblad(I)
+    n = len(ttn.nodes) // 2
+    norm = np.sqrt(norm ** (1/n))
+    for ket_id in list(ttn.nodes.keys())[0:len(ttn.nodes)//2]:
+        i, j = ket_id.replace('Site(', '').replace(')', '').split(',')
+        bra_id = f"Node({i},{j})"
+        T = ttn_normalized.tensors[ket_id].astype(complex)
+        T /= norm
+        ttn_normalized.tensors[ket_id] = T
+        ttn_normalized.nodes[ket_id].link_tensor(T)
+
+        T = ttn_normalized.tensors[bra_id].astype(complex)
+        T /= norm.conj()
+        ttn_normalized.tensors[bra_id] = T
+        ttn_normalized.nodes[bra_id].link_tensor(T)
+
+    return ttn_normalized
+
+#def normalize_ttn_Lindblad(ttn) : 
+    ttn_normalized = copy_object(ttn, deep = True)
+    I = TTNO.Identity(ttn_normalized)
+    for ket_id in list(ttn.nodes.keys())[0:len(ttn.nodes)//2]:
+        i, j = ket_id.replace('Site(', '').replace(')', '').split(',')
+        bra_id = f"Node({i},{j})"
+
         norm = ttn_normalized.operator_expectation_value_Lindblad(I)
-        ttn_normalized.tensors[node_id] /= np.sqrt(norm)
+        norm = np.sqrt(norm)
+
+        T = ttn_normalized.tensors[ket_id].astype(complex)
+        T /= norm
+        ttn_normalized.tensors[ket_id] = T
+        ttn_normalized.nodes[ket_id].link_tensor(T)
+
+        T = ttn_normalized.tensors[bra_id].astype(complex)
+        T /= norm.conj()
+        ttn_normalized.tensors[bra_id] = T
+        ttn_normalized.nodes[bra_id].link_tensor(T)
+
     return ttn_normalized
 
 TTNS = TreeTensorNetworkState